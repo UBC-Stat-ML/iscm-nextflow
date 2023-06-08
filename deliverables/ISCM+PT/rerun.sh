ee4243a57a197367fca0d35ada79cbbc37033f53
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-06-08 10:23:52 -0700

    Auto: add deliverables

--- deliverables/ISCM+PT/rerun.sh
+++ deliverables/ISCM+PT/rerun.sh
@@ -3,14 +3,6 @@ require("ggplot2")
 require("dplyr")
 require("stringr")
 
-read.csv("aggregated/multiRoundPropagation.csv.gz") %>%
-  mutate(model = str_replace(model, "[$]Builder", "")) %>% 
-  mutate(model = str_replace(model, ".*[.]", "")) %>% 
-  ggplot(aes(x = iteration, y = ess, colour = method, linetype = method)) +
-    geom_line()  + 
-    facet_grid(model~round, scales = "free") +
-    theme_minimal()
-ggsave("multiRoundPropagation-by-iteration.pdf", width = 35, height = 20, limitsize = FALSE)
 
 preds <- read.csv("aggregated/predictedResamplingInterval.csv.gz") %>%
   mutate(model = str_replace(model, "[$]Builder", "")) %>% 

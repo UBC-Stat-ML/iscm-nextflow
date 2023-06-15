c2a98ed91bde9191ec707e7a48cf2a24b38d8ca6
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-06-15 12:16:57 -0700

    Auto: add deliverables

--- deliverables/vary-n-expl/rerun.sh
+++ deliverables/vary-n-expl/rerun.sh
@@ -2,16 +2,18 @@
   require("ggplot2")
   require("dplyr")
   require("stringr")
+  require("scales")
   
-  
+  cc <- scales::seq_gradient_pal("grey", "black", "Lab")(seq(0,1,length.out=6))
   
   read.csv("aggregated/lambdaInstantaneous.csv.gz") %>%
     filter(isAdapt == "false") %>%
     mutate(model = str_replace(model, "[$]Builder", "")) %>% 
     mutate(model = str_replace(model, ".*[.]", "")) %>% 
-    ggplot(aes(x = beta, y = value, colour = log2(nPassesPerScan), group = factor(nPassesPerScan))) +
-      labs(color='log2 expected updates
+    ggplot(aes(x = beta, y = value, colour = factor(nPassesPerScan), group = factor(nPassesPerScan))) +
+      labs(color='Expected updates
 per exploration phase')  + 
+      scale_colour_manual(values=cc) +
       geom_line()  + 
       scale_y_continuous(expand = expansion(mult = 0.05), limits = c(0, NA)) +
       facet_wrap(~model, scales = "free_y") +

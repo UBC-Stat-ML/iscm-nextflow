2a0796b6a67086c5db7aab21fec62ea29449c1df
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-06-15 11:29:33 -0700

    Auto: add deliverables

--- deliverables/vary-n-expl/rerun.sh
+++ deliverables/vary-n-expl/rerun.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/env Rscript
-require("ggplot2")
-require("dplyr")
-require("stringr")
-
-
-
-read.csv("aggregated/lambdaInstantaneous.csv.gz") %>%
-  filter(isAdapt == "false") %>%
-  mutate(model = str_replace(model, "[$]Builder", "")) %>% 
-  mutate(model = str_replace(model, ".*[.]", "")) %>% 
-  ggplot(aes(x = beta, y = value, colour = factor(nPassesPerScan), linetype = factor(nPassesPerScan))) +
-    geom_line()  + 
-    scale_y_continuous(expand = expansion(mult = 0.05), limits = c(0, NA)) +
-    facet_wrap(~model, scales = "free_y") +
-    theme_minimal()
-ggsave("lambdaInstantaneous.pdf", width = 10, height = 5, limitsize = FALSE)
+  require("ggplot2")
+  require("dplyr")
+  require("stringr")
+  
+  
+  
+  read.csv("aggregated/lambdaInstantaneous.csv.gz") %>%
+    filter(isAdapt == "false") %>%
+    mutate(model = str_replace(model, "[$]Builder", "")) %>% 
+    mutate(model = str_replace(model, ".*[.]", "")) %>% 
+    ggplot(aes(x = beta, y = value, colour = log2(nPassesPerScan), group = factor(nPassesPerScan))) +
+      labs(color='log2 expected updates
+per exploration phase')  + 
+      geom_line()  + 
+      scale_y_continuous(expand = expansion(mult = 0.05), limits = c(0, NA)) +
+      facet_wrap(~model, scales = "free_y") +
+      theme_minimal()
+  ggsave("lambdaInstantaneous.pdf", width = 10, height = 5, limitsize = FALSE)

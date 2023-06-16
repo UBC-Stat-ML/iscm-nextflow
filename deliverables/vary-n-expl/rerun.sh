e6f936e9d62d2cf552d5323bccdac07293e3afa9
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2023-06-16 06:33:31 -0700

    Auto: add deliverables

--- deliverables/vary-n-expl/rerun.sh
+++ deliverables/vary-n-expl/rerun.sh
@@ -10,10 +10,16 @@
     filter(isAdapt == "false") %>%
     mutate(model = str_replace(model, "[$]Builder", "")) %>% 
     mutate(model = str_replace(model, ".*[.]", "")) %>% 
+    mutate(model = str_replace(model, "HierarchicalRockets", "glm")) %>% 
+    mutate(model = str_replace(model, "MRNATransfection", "ode")) %>% 
+    mutate(model = str_replace(model, "SimpleMixture", "mixture")) %>% 
+    mutate(model = str_replace(model, "SpikeSlabClassification", "spike-slab")) %>% 
+    mutate(model = str_replace(model, "XY", "rotor")) %>% 
     ggplot(aes(x = beta, y = value, colour = factor(nPassesPerScan), group = factor(nPassesPerScan))) +
       labs(color='Expected updates
 per exploration phase')  + 
       scale_colour_manual(values=cc) +
+      ylab("local barrier") + 
       geom_line()  + 
       scale_y_continuous(expand = expansion(mult = 0.05), limits = c(0, NA)) +
       facet_wrap(~model, scales = "free_y") +
